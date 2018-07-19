43ee751b2489fb0706bf25c5a77ecc4cea56faca
Tiffany Leigh <tiffanyleigh.10@gmail.com>
2018-07-19 07:12:17 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -3,6 +3,6 @@ def oxford_comma(array)
   return array[0] if array.length == 1
   return array[0..-2].join(', ') + ", and " + array[-1] if array.length > 1
 
-else array.length == 2
+else array.length == 3
   return array[0..-2].join(' ') + " and " + array[-1]
 end

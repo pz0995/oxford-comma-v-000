7191e105e4061ceeae556f7478fbc59736e80834
Tiffany Leigh <tiffanyleigh.10@gmail.com>
2018-07-19 05:27:26 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,23 @@
-def oxford_comma(array)
+def oxford_comma(fruit)
+fruit = ["kiwi", "durian", "starfruit", "mangos", "dragon fruits", "lychees", "pomelos"]*","
+
+fruit.join = reverse_array
+
+    reverse_array = []
+
+    i = string.length
+
+temp_array.push(string.split(""))
+
+temp_array.each do |item|
+
+    until i < 0
+        reverse_array.push(item[i])
+        i -= 1
+    end
+    #print reverse_array.inspect
+end
+
+reverse_array.join
 
 end

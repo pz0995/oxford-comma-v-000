4541e8b95e59e038851d6870c4860bef0c015511
Tiffany Leigh <tiffanyleigh.10@gmail.com>
2018-07-19 07:13:15 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,10 @@
 def oxford_comma(array)
+  if array.length == 2
+    return array[0..-2].join(' ') + " and " + array[-1]
+  else
+  return nil if array.nil?
+  return array[0] if array.length == 1
+  return array[0..-2].join(', ') + ", and " + array[-1] if array.length > 1
 
+end
 end

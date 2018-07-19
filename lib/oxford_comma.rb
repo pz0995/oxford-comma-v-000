a7c5e33338e5d51fbe73672db9a8e8bef062a782
Tiffany Leigh <tiffanyleigh.10@gmail.com>
2018-07-19 06:27:45 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,5 +1,4 @@
 def oxford_comma(array)
   return array.to_s if array.nil? or array.length <= 1
-  array[-1] = "and #{array[-1]}"
-  array.join(", ")
+    array[0..-2].join(", ") + " and " + array[-1]
 end
